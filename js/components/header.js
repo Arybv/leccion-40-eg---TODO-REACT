b6290297a49a4358fd42fd545280e5157c9c113c
Arantza Burga <arybv18@gmail.com>
2017-06-06 11:01:10 -0500

    en gh-pages

--- js/components/header.js
+++ js/components/header.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
   const Header = (update) => {
     const header = $('<header></header>');
-    const container = $('<div class = "container"></div>');
+    const container = $('<div class="container"></div>');
     const h1 = $('<h1>To Do App</h1>');
 
     container.append(h1);
